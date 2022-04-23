6afa25dc8dbb05f2f9ae875f6bde74796b8b1399
sanjarbek-ai <sanjarbekmohinaxon0205@gmail.com>
2022-04-23 12:47:25 +0500

    added

--- utils/db_api/update_comp.py
+++ utils/db_api/update_comp.py
@@ -20,7 +20,7 @@ async def update_comp_all_posts(message):
         query = user_post.update().values(
             status=UserPostStatus.disactive,
             updated_at=message.date
-        ).where(user_post.c.status == True)
+        ).where(user_post.c.status == UserPostStatus.accepted)
         await database.execute(query=query)
         return True
     except Exception as exc:
@@ -33,7 +33,7 @@ async def update_comp_image_uz(message, image):
         query = competitions.update().values(
             image_uz=image,
             updated_at=message.date
-        ).where(competitions.c.status == True)
+        ).where(competitions.c.status == CompetitionStatus.active)
         await database.execute(query=query)
         return True
     except Exception as exc:
@@ -46,7 +46,7 @@ async def update_comp_image_ru(message, image):
         query = competitions.update().values(
             image_ru=image,
             updated_at=message.date
-        ).where(competitions.c.status == True)
+        ).where(competitions.c.status == CompetitionStatus.active)
         await database.execute(query=query)
         return True
     except Exception as exc:
@@ -59,7 +59,7 @@ async def update_comp_conditions_uz(message, text):
         query = competitions.update().values(
             conditions_uz=text,
             updated_at=message.date
-        ).where(competitions.c.status == True)
+        ).where(competitions.c.status == CompetitionStatus.active)
         await database.execute(query=query)
         return True
     except Exception as exc:
@@ -72,7 +72,7 @@ async def update_comp_conditions_ru(message, text):
         query = competitions.update().values(
             conditions_ru=text,
             updated_at=message.date
-        ).where(competitions.c.status == True)
+        ).where(competitions.c.status == CompetitionStatus.active)
         await database.execute(query=query)
         return True
     except Exception as exc:
@@ -85,7 +85,7 @@ async def update_comp_gifts_image_uz(message, image):
         query = competitions.update().values(
             gifts_image_uz=image,
             updated_at=message.date
-        ).where(competitions.c.status == True)
+        ).where(competitions.c.status == CompetitionStatus.active)
         await database.execute(query=query)
         return True
     except Exception as exc:
@@ -98,7 +98,7 @@ async def update_comp_gifts_image_ru(message, image):
         query = competitions.update().values(
             gifts_image_ru=image,
             updated_at=message.date
-        ).where(competitions.c.status == True)
+        ).where(competitions.c.status == CompetitionStatus.active)
         await database.execute(query=query)
         return True
     except Exception as exc:
@@ -111,7 +111,7 @@ async def update_comp_gifts_uz(message, text):
         query = competitions.update().values(
             gifts_uz=text,
             updated_at=message.date
-        ).where(competitions.c.status == True)
+        ).where(competitions.c.status == CompetitionStatus.active)
         await database.execute(query=query)
         return True
     except Exception as exc:
@@ -124,7 +124,7 @@ async def update_comp_gifts_ru(message, text):
         query = competitions.update().values(
             gifts_ru=text,
             updated_at=message.date
-        ).where(competitions.c.status == True)
+        ).where(competitions.c.status == CompetitionStatus.active)
         await database.execute(query=query)
         return True
     except Exception as exc:
